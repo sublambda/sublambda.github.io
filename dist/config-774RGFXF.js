7ab73397b6af07a11c8b96b642547abfba0961e5
tk <zeroish@gmail.com>
2024-11-28 11:15:06 -0500

    .

---
+++ dist/config-774RGFXF.js
@@ -0,0 +1,1 @@
+import"./chunk-FSRPMVAS.js";var g={name:"fuck",pass:"aym4NwIKf5RWIig-kfWw2Q",appId:"com.gmail.zeroish.jerk",apiKey:"11AxeqxgNgmDnbBOgP867IG08ghJzFcsHhtHDgjAs"};export{g as config};
---
+++ dist/config.js
@@ -0,0 +1,10 @@
+
+var config = {
+  // croquet
+  name: "fuck",
+  pass: "aym4NwIKf5RWIig-kfWw2Q",
+  appId: 'com.gmail.zeroish.jerk',
+  apiKey: '11AxeqxgNgmDnbBOgP867IG08ghJzFcsHhtHDgjAs',
+
+};
+export { config };
